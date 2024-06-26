8b053bbc80a63327ad54f9f31ea12bc0b33c021f
jjpp43 <jparkdev63@gmail.com>
2024-06-26 15:53:14 +0900

    Change in how it serves the image file

--- my-project/src/app/frames/route.tsx
+++ my-project/src/app/frames/route.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-key */
 import { Button, fetchMetadata } from 'frames.js/next';
 import { frames } from "./frames";
+import Image from 'next/image';
 
 
 
@@ -8,9 +9,9 @@ const frameHandler = frames(async (ctx) => {
 
     return {
         //
-        image: '/background.png',
+        image: <Image src={'/background.png'} alt={''} />,
         buttons: [
-            <Button action="post" target="/status">
+            <Button action="post" target="/tatus">
                 Check status
             </Button>,
         ],
